a411b74e9d415e37726be0592c1e1b2d9c0ab8c9
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2024-02-20 11:19:13 +0200

    Show/cancel new project form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectsSelected from "./components/NoProjectsSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return { ...prevState, selectedProjectId: null };
+    });
+  };
+
+  const cancelNewProjectFormHandler = () => {
+    setProjectsState((prevState) => {
+      return { ...prevState, selectedProjectId: undefined };
+    });
+  };
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject onCancelNewProjectForm={cancelNewProjectFormHandler} />
+    );
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectsSelected onStartAddProject={handleStartAddProject} />;
+  } else {
+    content = "Show selected project TBC";
+  }
+
   return (
     <main className=" h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      {/* <NewProject /> */}
-      <NoProjectsSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,12 +1,15 @@
 import Input from "./Input.jsx";
 import React from "react";
 
-const NewProject = () => {
+const NewProject = ({ onCancelNewProjectForm }) => {
   return (
     <div className="w-[35rem] mt-16 ">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-950 ">
+          <button
+            onClick={onCancelNewProjectForm}
+            className="text-stone-800 hover:text-stone-950 "
+          >
             Cancel
           </button>
         </li>
